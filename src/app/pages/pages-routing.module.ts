e86a289d6966c9f54af43865fb22f86a5c0acc9e
AkMANZHI <abhishek.manjhi@aryavratinfotech.com>
2019-08-19 18:48:57 +0530

    WIP

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,82 +1,91 @@
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
+//import { AppRoutingModule } from '../app-routing.module';
 
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+//import { ECommerceComponent } from './e-commerce/e-commerce.component';
+// import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
+ import { LoginComponent } from './login/login.component';
 import { ECommerceComponent } from './e-commerce/e-commerce.component';
-import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
+ 
+
 
 const routes: Routes = [{
   path: '',
   component: PagesComponent,
   children: [
+    // {
+    //   path: 'e-commerce',
+    //   component: ECommerceComponent
+    // },
     {
-      path: 'dashboard',
-      component: ECommerceComponent,
+      path: 'login',
+      component: LoginComponent,
     },
-    {
-      path: 'iot-dashboard',
-      component: DashboardComponent,
-    },
-    {
-      path: 'layout',
-      loadChildren: () => import('./layout/layout.module')
-        .then(m => m.LayoutModule),
-    },
-    {
-      path: 'forms',
-      loadChildren: () => import('./forms/forms.module')
-        .then(m => m.FormsModule),
-    },
-    {
-      path: 'ui-features',
-      loadChildren: () => import('./ui-features/ui-features.module')
-        .then(m => m.UiFeaturesModule),
-    },
-    {
-      path: 'modal-overlays',
-      loadChildren: () => import('./modal-overlays/modal-overlays.module')
-        .then(m => m.ModalOverlaysModule),
-    },
-    {
-      path: 'extra-components',
-      loadChildren: () => import('./extra-components/extra-components.module')
-        .then(m => m.ExtraComponentsModule),
-    },
-    {
-      path: 'maps',
-      loadChildren: () => import('./maps/maps.module')
-        .then(m => m.MapsModule),
-    },
-    {
-      path: 'charts',
-      loadChildren: () => import('./charts/charts.module')
-        .then(m => m.ChartsModule),
-    },
-    {
-      path: 'editors',
-      loadChildren: () => import('./editors/editors.module')
-        .then(m => m.EditorsModule),
-    },
-    {
-      path: 'tables',
-      loadChildren: () => import('./tables/tables.module')
-        .then(m => m.TablesModule),
-    },
-    {
-      path: 'miscellaneous',
-      loadChildren: () => import('./miscellaneous/miscellaneous.module')
-        .then(m => m.MiscellaneousModule),
-    },
+    // {
+    //   path: 'iot-dashboard',
+    //   component: DashboardComponent,
+    // },
+    // {
+    //   path: 'layout',
+    //   loadChildren: () => import('./layout/layout.module')
+    //     .then(m => m.LayoutModule),
+    // },
+    // {
+    //   path: 'forms',
+    //   loadChildren: () => import('./forms/forms.module')
+    //     .then(m => m.FormsModule),
+    // },
+    // {
+    //   path: 'ui-features',
+    //   loadChildren: () => import('./ui-features/ui-features.module')
+    //     .then(m => m.UiFeaturesModule),
+    // },
+    // {
+    //   path: 'modal-overlays',
+    //   loadChildren: () => import('./modal-overlays/modal-overlays.module')
+    //     .then(m => m.ModalOverlaysModule),
+    // },
+    // {
+    //   path: 'extra-components',
+    //   loadChildren: () => import('./extra-components/extra-components.module')
+    //     .then(m => m.ExtraComponentsModule),
+    // },
+    // {
+    //   path: 'maps',
+    //   loadChildren: () => import('./maps/maps.module')
+    //     .then(m => m.MapsModule),
+    // },
+    // {
+    //   path: 'charts',
+    //   loadChildren: () => import('./charts/charts.module')
+    //     .then(m => m.ChartsModule),
+    // },
+    // {
+    //   path: 'editors',
+    //   loadChildren: () => import('./editors/editors.module')
+    //     .then(m => m.EditorsModule),
+    // },
+    // {
+    //   path: 'tables',
+    //   loadChildren: () => import('./tables/tables.module')
+    //     .then(m => m.TablesModule),
+    // },
+    // {
+    //   path: 'miscellaneous',
+    //   loadChildren: () => import('./miscellaneous/miscellaneous.module')
+    //     .then(m => m.MiscellaneousModule),
+    // },
     {
       path: '',
-      redirectTo: 'dashboard',
+      redirectTo: 'login',
       pathMatch: 'full',
-    },
-    {
-      path: '**',
-      component: NotFoundComponent,
-    },
+    }
+    // {
+    //   path: '**',
+    //   component: NotFoundComponent,
+    // },   
   ],
 }];
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
-import { NbMenuModule } from '@nebular/theme';
+import { NbMenuModule, NbAlertComponent } from '@nebular/theme';
+
 
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
@@ -7,6 +8,7 @@ import { DashboardModule } from './dashboard/dashboard.module';
 import { ECommerceModule } from './e-commerce/e-commerce.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
@@ -16,9 +18,14 @@ import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
     DashboardModule,
     ECommerceModule,
     MiscellaneousModule,
+   
+
+    
   ],
   declarations: [
     PagesComponent,
+    LoginComponent,
+    NbAlertComponent
   ],
 })
 export class PagesModule {
